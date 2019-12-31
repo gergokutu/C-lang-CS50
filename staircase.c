eafb3293eb386bb27c6c2520e6b1635dd50bcc07
gergokutu <gergo.kutu@gmail.com>
2019-12-31 11:16:44 +0100

    Input from user from 1 to 8 - staircase.c

--- staircase.c
+++ staircase.c
@@ -1,42 +1,45 @@
-// #include <cs50.h>
 #include <stdio.h>
 
-// int get_positive(void);
+int get_positive(void);
 void build_pyramid(int height);
 
 int main(void)
 {
-    // int height = get_positive();
-    int height = 8;
-    printf("Height: %i\n", height);
-    build_pyramid(height);
+  int height = get_positive();
+  printf("Height: %i\n", height);
+  build_pyramid(height);
 }
 
-// int get_positive(void)
-// {
-//     int number;
-    
-//     do
-//     {
-//        number = get_int("Ensure the height of thy pyramid /1-8/: ");
-//     }
-//     while ( number < 1 || number > 8 );
-    
-//     return number;
-// }
+int get_positive(void)
+{
+  // Get input from user
+  // int number;
+  // printf("Ensure the height of thy pyramid /1-8/: ");
+  // scanf("%d", &number);
+  int number;
+
+  do
+  {
+    printf("Ensure the height of thy pyramid /1-8/: ");
+    scanf("%d", &number);
+  }
+  while ( number < 1 || number > 8 );
+
+  return number;
+}
 
 void build_pyramid(int height)
 {
-    for (int i = 0; i < height; i++)
-    {   
-        for (int space = (height - i); space > 0; space--)
-        {
-            printf(" ");
-        }
-        for (int hash = 0; hash < i + 1; hash++)
-        {
-            printf("#"); // prints the hashtags
-        }
-        printf("\n");
+  for (int i = 0; i < height; i++)
+  {
+    for (int space = (height - i); space > 0; space--)
+    {
+      printf(" ");
     }
+    for (int hash = 0; hash < i + 1; hash++)
+    {
+      printf("#");
+    }
+    printf("\n");
+  }
 }
