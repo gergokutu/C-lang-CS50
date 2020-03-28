ff986d5bc1efc2f87396d6a3ff390d2b6ba8c172
gergokutu <gergo.kutu@gmail.com>
2020-03-28 15:49:45 +0100

    Implement to_uppercase with using the ASCII codes

---
+++ week2/to_uppercase.c
@@ -0,0 +1,29 @@
+// Uppercases a string
+// think about the ASCII codes
+// 'A' is 32 before 'a'...
+
+#include <stdio.h>
+#include <string.h>
+
+const int N = 10;
+
+int main(void)
+{
+    char str[N];
+    printf("Input: ");
+    scanf("%s", str);
+
+    printf("After:  ");
+    for (int i = 0, n = strlen(str); i < n; i++)
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            printf("%c", str[i] - 32);
+        }
+        else
+        {
+            printf("%c", str[i]);
+        }
+    }
+    printf("\n");
+}
