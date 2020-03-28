458af1f119c734cafb4080a84bee6f6284920d75
gergokutu <gergo.kutu@gmail.com>
2020-03-28 14:07:10 +0100

    Calculate the average of 3 numbers in an array, const used for array size

---
+++ week2/average.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+const int N = 3;
+
+int main(void)
+{
+  int scores[N] = {72, 73, 33};
+
+  // scores[0] = 72;
+  // scores[1] = 73;
+  // scores[2] = 33;
+
+  // if the dividend or the divisor is cast to float that's enough to have a float as a result...
+  printf("The average is %.2f\n", (float) (scores[0] + scores[1] + scores[2]) / (float) N );
+
+  return 0;
+}
