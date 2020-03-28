0ead2528d9c137d78ce722a18b1b344e0475d082
gergokutu <gergo.kutu@gmail.com>
2020-03-28 13:49:50 +0100

    Format output

--- week2/print_ASCII.c
+++ week2/print_ASCII.c
@@ -6,7 +6,14 @@ int main(void)
   char c2 = 'I';
   char c3 = '!';
 
-  printf("H: %i\nI: %i\n!: %i\n", c1, c2, c3);
+  printf("%c %c %c\n", c1, c2, c3);
+
+  // casting > force the datatype
+  // handle c1, c2, c3 as int
+  printf("\nH: %i\nI: %i\n!: %i\n\n", (int) c1, (int) c2, (int) c3);
+
+  // if you do not cast the datatype it is done implicitly
+  printf("%i %i %i\n", c1, c2, c3);
 
   return 0;
 }
