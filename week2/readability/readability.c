e6e64dc57fad2d9906cfa872c17a63f4891f6ef8
gergokutu <gergo.kutu@gmail.com>
2020-03-31 12:52:49 +0200

    Apply cs50 coding style format

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -6,70 +6,81 @@ const int N = 100000;
 
 int main(void)
 {
-  char paragraph[N];
+    char paragraph[N];
 
-  printf("Enter your paragraph: ");
-  // scanf("%s", paragraph);
-  // accept a paragraph as input
-  fgets(paragraph, N, stdin);
-  printf("\n************\n\n");
-  printf("The ensured paragraph is:\n\n%s\n", paragraph);
+    printf("Text: ");
 
-  // count the letters, words and sentences in the paragraph
-  // think about the ASCII codes!
-  int letters = 0;
-  // words start from zero => Hey! or hey
-  // 1 word but not a sentence
-  int words = 1;
-  int sentences = 0;
+    // accept a paragraph as input
+    fgets(paragraph, N, stdin);
+    printf("\n************\n\n");
+    printf("The ensured paragraph is:\n\n%s\n", paragraph);
 
-  for (int i = 0; i < strlen(paragraph); i++)
-  {
-    // letters
-    if (paragraph[i] >= 'a' && paragraph[i] <= 'z') letters++;
-    if (paragraph[i] >= 'A' && paragraph[i] <= 'Z') letters++;
+    // count the letters, words and sentences in the paragraph
+    // think about the ASCII codes!
+    int letters = 0;
+    // words start from zero => Hey! or hey
+    // 1 word but not a sentence
+    int words = 1;
+    int sentences = 0;
 
-    // words
-    if (paragraph[i] == ' ') words++;
+    for (int i = 0; i < strlen(paragraph); i++)
+    {
+        // letters
+        if (paragraph[i] >= 'a' && paragraph[i] <= 'z')
+        {
+            letters++;
+        }
+        if (paragraph[i] >= 'A' && paragraph[i] <= 'Z')
+        {
+            letters++;
+        }
+        // words
+        if (paragraph[i] == ' ')
+        {
+            words++;
+        }
+        // sentences
+        if ((paragraph[i] == '.' || paragraph[i] == '!' || paragraph[i] == '?')
+            && paragraph[i - 1] != '.'
+            && paragraph[i - 1] != '!'
+            && paragraph[i - 1] != '?')
+        {
+            sentences++;
+        }
+    }
 
-    // sentences
-    if ((paragraph[i] == '.' || paragraph[i] == '!' || paragraph[i] == '?') 
-      && paragraph[i - 1] != '.' 
-      && paragraph[i - 1] != '!' 
-      && paragraph[i - 1] != '?') sentences++;
-  }
+    // User feedback about the details
+    printf("************\n\n");
+    printf("Results: \n");
+    printf("Letters: %d\n", letters);
+    printf("Words: %d\n", words);
+    printf("Sentences: %d\n\n", sentences);
 
-  // User feedback about the details
-  printf("************\n\n");
-  printf("Results: \n");
-  printf("Letters: %d\n", letters);
-  printf("Words: %d\n", words);
-  printf("Sentences: %d\n\n", sentences);
+    // apply the Coleman-Liau index
+    float L = letters * 100 / words;
+    float S = sentences * 100 / words;
+    float index = 0.0588 * L - 0.296 * S - 15.8;
 
-  // apply the Coleman-Liau index
-  float L = letters * 100 / words;
-  float S = sentences * 100 / words;
-  float index = 0.0588 * L - 0.296 * S - 15.8;
+    printf("INDEX: %f\n", index);
 
-  printf("INDEX: %f\n", index);
+    // round the result
+    int grade = round(index);
 
-  // round the result
-  int grade = round(index);
-  
-  // ensure proper feedback on the index
-  if (grade < 1) {
-    printf("Before GRADE 1\n");
-  }
-  else if (grade >= 16)
-  {
-    printf("GRADE 16+\n");
-  }
-  else
-  {
-    printf("GRADE %d\n", grade);
-  }
+    // ensure proper feedback on the index
+    if (grade < 1)
+    {
+        printf("Before GRADE 1\n");
+    }
+    else if (grade >= 16)
+    {
+        printf("GRADE 16+\n");
+    }
+    else
+    {
+        printf("GRADE %d\n", grade);
+    }
 
-  printf("\n************\n\n");
-  
-  return 0;
+    printf("\n************\n\n");
+
+    return 0;
 }
