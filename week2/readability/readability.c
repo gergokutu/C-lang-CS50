eb19217ec532363418202539a8743f76efa3f507
gergokutu <gergo.kutu@gmail.com>
2020-03-31 11:39:54 +0200

    Count letters, words, sentences in the input paragraph

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,14 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+
+const int N = 100000;
 
 int main(void)
 {
-  char paragraph[100000];
+  char paragraph[N];
 
   printf("Enter your paragraph: ");
   // scanf("%s", paragraph);
-  fgets(paragraph, 100000, stdin);
+  // accept a paragraph as input
+  fgets(paragraph, N, stdin);
 
   printf("The ensured paragraph is:\n\n%s\n", paragraph);
+
+  // count the letters, words and sentences in the paragraph
+  // think about the ASCII codes!
+  int letters;
+  // words start from zero => Hey! or hey
+  // 1 word but not a sentence
+  int words = 1;
+  int sentences;
+
+  for (int i = 0; i < strlen(paragraph); i++)
+  {
+    // letters
+    if (paragraph[i] >= 'a' && paragraph[i] <= 'z') letters++;
+    if (paragraph[i] >= 'A' && paragraph[i] <= 'Z') letters++;
+
+    // words
+    if (paragraph[i] == ' ') words++;
+
+    // sentences
+    if ((paragraph[i] == '.' || paragraph[i] == '!' || paragraph[i] == '?') 
+      && paragraph[i - 1] != '.' 
+      && paragraph[i - 1] != '!' 
+      && paragraph[i - 1] != '?') sentences++;
+  }
+
+  printf("Letters: %d\n", letters);
+  printf("Words: %d\n", words);
+  printf("Sentences: %d\n", sentences);
   
   return 0;
 }
