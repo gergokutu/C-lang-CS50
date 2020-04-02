96925434186c322b317de6ba89a507bd27d77804
gergokutu <gergo.kutu@gmail.com>
2020-04-02 10:52:50 +0200

    Validate that key contains only letters

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 // accept a single command-line argument
 int main(int argc, char **argv)
@@ -13,10 +14,22 @@ int main(int argc, char **argv)
 
   // Check if the key is 26 char long
   char *key = argv[1];
-  if (strlen(key) != 26)
+  int key_length = strlen(key);
+  if (key_length != 26)
   {
-    printf("Usage: ./substitution key\nKey should be 26 characters long\n");
+    printf("Usage: ./substitution key\nKey should be 26 characters long!\n");
     return 1;
+  }
+
+  // Check if the key consists of only letters - case-insensitive
+  for (int i = 0; i < key_length; i++)
+  {
+    char letter = toupper(key[i]);
+    if (letter < 'A' || letter > 'Z')
+    {
+      printf("Usage: ./substitution key\nKey should contain only letters!\n");
+      return 1;
+    }
   }
 
   printf("Success\n");
