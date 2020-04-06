b9da043c23fa7b69ffea336cd1021cefe2767a14
gergokutu <gergo.kutu@gmail.com>
2020-04-06 16:13:49 +0200

    Implement recursive pyramid builder

---
+++ week3/recursion.c
@@ -0,0 +1,33 @@
+// Build pyramid
+// Instead of nested for loop >
+// Design it the recursive way
+#include <stdio.h>
+
+void draw(int h);
+
+int main(void)
+{
+  int height;
+  printf("Height: ");
+  scanf("%d", &height);
+
+  draw(height);
+}
+
+void draw(int h)
+{
+  // If nothing to draw > exit
+  if (h == 0)
+  {
+    return;
+  }
+
+  // Draw pyramid with height - 1
+  draw(h - 1);
+
+  for (int i = 0; i < h; i++)
+  {
+    printf("#");
+  }
+  printf("\n");
+}
