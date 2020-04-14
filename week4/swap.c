1741354cc260eb0a11a0c14ea5f7970576ff1e0d
gergokutu <gergo.kutu@gmail.com>
2020-04-14 14:55:29 +0200

    Comments

--- week4/swap.c
+++ week4/swap.c
@@ -8,10 +8,15 @@ int main(void)
     int y = 2;
 
     printf("x is %i, y is %i\n", x, y);
+    // pass the address
     swap(&x, &y);
     printf("x is %i, y is %i\n", x, y);
 }
 
+// define that the arguments are pointers
+// int *a connected to &x (by memory address)
+// changing the value of a will change the value of x
+// because they have the same memory address
 void swap(int *a, int *b)
 {
     int tmp = *a;
